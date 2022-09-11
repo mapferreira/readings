729da3a3910bdbd1cbcb7afbbc6d64eeed37543b
Miguel Ferreira <miguelapferreira@protonmail.com>
2022-09-11 20:36:23 +0200

    hello

---
+++ src/main/java/com/domike/hello/Hello.java
@@ -0,0 +1,13 @@
+package com.domike.hello;
+
+public class Hello {
+        public String message;
+
+    public Hello() {
+    }
+
+        Hello(String message){
+            this.message = message;
+        }
+
+}
